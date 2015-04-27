5d7c7111a6fbbc9727c09c82e1c2641399a55e32
Steven Armstrong <steven.armstrong@gmail.com>
2015-04-27 14:27:36 -0400

    fixed date value in test

--- src/test/java/io/github/codemumbler/datatype/DateTimeTest.java
+++ src/test/java/io/github/codemumbler/datatype/DateTimeTest.java
@@ -3,14 +3,21 @@ package io.github.codemumbler.datatype;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class DateTimeTest {
 
 	@Test
 	public void wrapDateObject() {
 		DateTime dateTime = new DateTime();
-		Date date = new Date(1234567890000L);
-		Assert.assertEquals("TO_TIMESTAMP('02/13/2009 18:31:30', 'MM/DD/YYYY HH24:MI:SS')", dateTime.writeValue(date));
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.MONTH, 0);
+		calendar.set(Calendar.DAY_OF_MONTH, 2);
+		calendar.set(Calendar.YEAR, 2015);
+		calendar.set(Calendar.HOUR_OF_DAY, 23);
+		calendar.set(Calendar.MINUTE, 16);
+		calendar.set(Calendar.SECOND, 7);
+		Assert.assertEquals("TO_TIMESTAMP('01/02/2015 23:16:07', 'MM/DD/YYYY HH24:MI:SS')",
+				dateTime.writeValue(calendar.getTime()));
 	}
 }
