adf8085080fca5c87f8fba8ac00e2d5733013963
Steven Armstrong <steven.armstrong@gmail.com>
2015-04-26 22:21:37 -0400

    cleaned up some possible ACCESS names

--- src/main/java/io/github/codemumbler/OracleScriptWriter.java
+++ src/main/java/io/github/codemumbler/OracleScriptWriter.java
@@ -1,10 +1,17 @@
 package io.github.codemumbler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OracleScriptWriter {
 
 	private static final String SCHEMA_CREATION = "--CREATE USER %1$s IDENTIFIED BY password2Change;\n" +
 			"-- GRANT CONNECT, RESOURCE, CREATE SESSION, CREATE TABLE, CREATE VIEW,\n" +
 			"-- \tCREATE PROCEDURE,CREATE SYNONYM, CREATE SEQUENCE, CREATE TRIGGER TO %1$s;\n";
+	private static final List<String> RESERVED_WORDS = Arrays.asList(new String[]{
+			"SQL", "GROUP", "LANGUAGE", "BY", "DECLARE", "SELECT", "WHERE", "FROM"
+	});
 	private final Database database;
 
 	public OracleScriptWriter(Database database) {
@@ -25,5 +32,38 @@ public class OracleScriptWriter {
 		}
 		tableCreateScript.append(");");
 		return tableCreateScript.toString();
+	}
+
+	public String cleanName(String name) {
+		if ( RESERVED_WORDS.contains(name.toUpperCase()) )
+			return name.toUpperCase() + "_";
+		if ( name.equals(name.toUpperCase()) )
+			return name;
+		name = name.replaceAll(" ", "_");
+		name = name.replaceAll("[\\(\\)\\\\/#]", "");
+		List<Boolean> upperCase = new ArrayList<>(name.length());
+		for ( int i = 0; i < name.length(); i++ ) {
+			upperCase.add(Character.isUpperCase(name.charAt(i)));
+		}
+		for ( int index = 1; index < name.length(); index++ ) {
+			if ( upperCase.get(index) && index < name.length() - 1 ) {
+				if (index + 1 < name.length() - 1 && upperCase.get(index + 1))
+					continue;
+				List<Boolean> uppers = upperCase.subList(index-1, upperCase.size()-1);
+				if (name.charAt(index-1) != '_' && !isOnlyUpperCase(uppers)) {
+					name = name.substring(0, index) + "_" + name.substring(index);
+					upperCase.add(index + 1, false);
+				}
+			}
+		}
+		return name.toUpperCase();
+	}
+
+	private boolean isOnlyUpperCase(List<Boolean> uppers) {
+		for ( Boolean bool : uppers ) {
+			if ( !bool )
+				return false;
+		}
+		return true;
 	}
 }
--- src/test/java/io/github/codemumbler/OracleScriptWriterTest.java
+++ src/test/java/io/github/codemumbler/OracleScriptWriterTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class OracleScriptWriterTest {
@@ -22,8 +23,8 @@ public class OracleScriptWriterTest {
 	public void convertEmptySchema() {
 		database.setSchemaName("EmptySchema");
 		Assert.assertEquals("--CREATE USER EmptySchema IDENTIFIED BY password2Change;\n" +
-				"-- GRANT CONNECT, RESOURCE, CREATE SESSION, CREATE TABLE, CREATE VIEW,\n" +
-				"-- \tCREATE PROCEDURE,CREATE SYNONYM, CREATE SEQUENCE, CREATE TRIGGER TO EmptySchema;\n",
+						"-- GRANT CONNECT, RESOURCE, CREATE SESSION, CREATE TABLE, CREATE VIEW,\n" +
+						"-- \tCREATE PROCEDURE,CREATE SYNONYM, CREATE SEQUENCE, CREATE TRIGGER TO EmptySchema;\n",
 				writer.writeScript());
 	}
 
@@ -39,5 +40,55 @@ public class OracleScriptWriterTest {
 		Assert.assertEquals("CREATE TABLE TEST_TABLE (\n" +
 				"\tTEST_COLUMN_ID NUMBER(5)\n" +
 				");", writer.writeOneTable(table));
+	}
+
+	@Test
+	public void cleanName_alreadyUpperCase() {
+		Assert.assertEquals("TABLE1", writer.cleanName("TABLE1"));
+	}
+
+	@Test
+	public void cleanName_toUpperCase() {
+		Assert.assertEquals("PROJECTS", writer.cleanName("projects"));
+	}
+
+	@Test
+	public void cleanName_separateCamelCase() {
+		Assert.assertEquals("TEST_SCHEMA", writer.cleanName("testSchema"));
+	}
+
+	@Test
+	public void cleanName_retainAcronymsBeforeWord() {
+		Assert.assertEquals("EMP_LEVEL", writer.cleanName("EMPLevel"));
+	}
+
+	@Test
+	public void cleanName_retainAcronymsAfterWord() {
+		Assert.assertEquals("ACCESS_LEVEL_ID", writer.cleanName("AccessLevelID"));
+	}
+
+	@Test
+	public void cleanName_DoNotAddExtraUnderscores() {
+		Assert.assertEquals("ACCESS_LEVEL_ID", writer.cleanName("Access_LevelID"));
+	}
+
+	@Test
+	public void cleanName_removeSpacesAndBrackets() {
+		Assert.assertEquals("NAME_FULL", writer.cleanName("Name (Full)"));
+	}
+
+	@Test
+	public void cleanName_secondWordAcronym() {
+		Assert.assertEquals("NEW_EMP", writer.cleanName("New EMP"));
+	}
+
+	@Test
+	public void cleanName_AddUnderscoreToReservedWords() {
+		Assert.assertEquals("SQL_", writer.cleanName("SQL"));
+	}
+
+	@Test
+	public void cleanName_upperCaseReservedWords() {
+		Assert.assertEquals("GROUP_", writer.cleanName("group"));
 	}
 }
