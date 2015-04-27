fe6c3d8ec3dc7ac16c893063c9a7653b98591ee8
Steven Armstrong <steven.armstrong@gmail.com>
2015-04-26 21:38:07 -0400

    writes the SQL script for one simple table

--- src/test/java/io/github/codemumbler/OracleScriptWriterTest.java
+++ src/test/java/io/github/codemumbler/OracleScriptWriterTest.java
@@ -1,18 +1,43 @@
 package io.github.codemumbler;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class OracleScriptWriterTest {
 
+	private Database database;
+	private OracleScriptWriter writer;
+
+	@Before
+	public void setUp() {
+		database = new Database();
+		writer = new OracleScriptWriter(database);
+		database.setSchemaName("TEST_SCHEMA");
+	}
+
 	@Test
 	public void convertEmptySchema() {
-		Database database = new Database();
 		database.setSchemaName("EmptySchema");
-		OracleScriptWriter writer = new OracleScriptWriter(database);
 		Assert.assertEquals("--CREATE USER EmptySchema IDENTIFIED BY password2Change;\n" +
 				"-- GRANT CONNECT, RESOURCE, CREATE SESSION, CREATE TABLE, CREATE VIEW,\n" +
 				"-- \tCREATE PROCEDURE,CREATE SYNONYM, CREATE SEQUENCE, CREATE TRIGGER TO EmptySchema;\n",
 				writer.writeScript());
+	}
+
+	@Test
+	public void oneTableColumns() {
+		Table table = new Table();
+		table.setName("TEST_TABLE");
+		Column column = new Column();
+		column.setName("TEST_COLUMN_ID");
+		column.setDataType(DataType.INTEGER);
+		column.setLength(5);
+		table.addColumn(column);
+		Assert.assertEquals("CREATE TABLE TEST_TABLE (\n" +
+				"\tTEST_COLUMN_ID NUMBER(5)\n" +
+				");", writer.writeOneTable(table));
 	}
 }
