655caac297c25315cec52d4f6c9d6935b8ff9f19
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-06-20 08:01:33 -0400

    is it in the download directory

--- .travis/mvn-ojdbc.sh
+++ .travis/mvn-ojdbc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.1 -Dpackaging=jar -Dfile=ojdbc6.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.1 -Dpackaging=jar -Dfile=.travis/ojdbc6.jar -DgeneratePom=true
